fea86f59a8f0bffe26ca5378772134d57ee5c2b4
Lucas de Sousa Nunes <lucas103101@gmail.com>
2025-07-24 22:44:19 -0300

    add postgres

--- src/settings.py
+++ src/settings.py
@@ -1,7 +1,7 @@
 from decouple import config
 
 APPLICATION_NAME = config("APPLICATION_NAME", cast=str, default="MyApp")
-DATABASE_URL = config("DATABASE_URL", cast=str, default="sqlite:///./data.db")
+DATABASE_URL = config("DATABASE_URL", cast=str, default="")
 REDIS_URL = config("REDIS_URL", cast=str, default="redis://localhost:6379/0")
 KINESIS_ENDPOINT_URL = config("KINESIS_ENDPOINT_URL", cast=str, default="http://localhost:4566")
 KINESIS_EVENTS_STREAM_REGION = config("KINESIS_EVENTS_STREAM_REGION", cast=str, default="us-east-1")
