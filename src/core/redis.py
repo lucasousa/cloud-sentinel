a8e609c47f18e015ccddddadada2a8a8e1aca4a9
Lucas de Sousa Nunes <lucas103101@gmail.com>
2025-07-26 15:47:43 -0300

    redis ssl

--- src/core/redis.py
+++ src/core/redis.py
@@ -1,4 +1,4 @@
-import ssl
+
 from functools import lru_cache
 from typing import Optional
 
@@ -13,35 +13,18 @@ class RedisClient:
         self.redis_url: AnyUrl = redis_url or REDIS_URL
         self.redis: Optional[redis.Redis] = None
 
-    def get_ssl_context(self) -> Optional[ssl.SSLContext]:
-        if self.redis_url.startswith("rediss://"):
-            return ssl.create_default_context()
-        return None
-
     def connect(self) -> redis.Redis:
-        params = self.get_params_from_url()
-        if not self.redis:
-            self.redis = redis.Redis(
-                host=params.get("host", "localhost"),
-                port=params.get("port", 6379),
-                ssl=True,
-                ssl_ca_certs=None,
-            )
+        if self.redis_url.startswith('rediss://'):
+            self.redis = redis.from_url(
+                self.redis_url, ssl_cert_reqs=None)
+        else:
+            self.redis = redis.from_url(self.redis_url)
         return self.redis
 
     async def close(self):
         if self.redis:
             await self.redis.close()
             self.redis = None
-
-    def get_params_from_url(self) -> dict:
-        url = AnyUrl(self.redis_url)
-        params = {
-            "host": url.host,
-            "port": url.port
-        }
-        return {k: v for k, v in params.items() if v is not None}
-
 
 @lru_cache()
 def get_redis_client() -> RedisClient:
