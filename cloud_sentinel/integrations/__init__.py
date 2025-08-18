bb12550be4f37503e65e8bc5a88ac7b08308a43a
Lucas de Sousa Nunes <lucas103101@gmail.com>
2025-08-18 14:19:57 -0300

    patch redis on

--- cloud_sentinel/integrations/__init__.py
+++ cloud_sentinel/integrations/__init__.py
@@ -1,9 +1,11 @@
 from .httpx import patch_httpx
 from .postgres import patch_tortoise_postgres
+from .redis import patch_redis
 from .vm import collect_vm_metrics_and_report
 
 
 async def patch_all_integrations():
+    patch_redis()
     patch_httpx()
     await collect_vm_metrics_and_report()
     patch_tortoise_postgres()
